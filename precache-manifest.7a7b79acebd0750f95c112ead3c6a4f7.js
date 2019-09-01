b2474d116ab4cdb8f3c81c829a28527fd817f7a4
unknown <dafuq.ahd@gmail.com>
2019-09-01 10:41:28 +0300

    Updates

---
+++ precache-manifest.7a7b79acebd0750f95c112ead3c6a4f7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afec884029da3c137e7a9cbbd45b2e6e",
+    "url": "/rasmi/index.html"
+  },
+  {
+    "revision": "462886ce64f18496b912",
+    "url": "/rasmi/static/css/main.f8af3253.chunk.css"
+  },
+  {
+    "revision": "33a1eaba26817fe2e53d",
+    "url": "/rasmi/static/js/2.7ab74bd2.chunk.js"
+  },
+  {
+    "revision": "462886ce64f18496b912",
+    "url": "/rasmi/static/js/main.b2d3e00c.chunk.js"
+  },
+  {
+    "revision": "cc365e840002f0b97642",
+    "url": "/rasmi/static/js/runtime~main.678e7461.js"
+  },
+  {
+    "revision": "c0da3006adeab3250825bd7cd246c199",
+    "url": "/rasmi/static/media/1.c0da3006.jpg"
+  },
+  {
+    "revision": "4a4bebbc7b3f56f3762ca09e1b5a64fa",
+    "url": "/rasmi/static/media/3.4a4bebbc.jpg"
+  },
+  {
+    "revision": "af7290e41fe96057ca7d44445c62686a",
+    "url": "/rasmi/static/media/4.af7290e4.jpg"
+  },
+  {
+    "revision": "b8966d68fe45124fd2146b05b625c7ed",
+    "url": "/rasmi/static/media/5.b8966d68.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rasmi/static/media/logo.5d5d9eef.svg"
+  }
+]);
